a24a7930cb82d871c15e98a048035f33a241673a
Karan Shetty <shettykaran2110@gmail.com>
2021-10-29 16:02:12 +0530

    Connect to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const PORT = 8080
 
@@ -7,6 +8,15 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+const connect = async () => {
+  await mongoose.connect(
+    'mongodb+srv://shettykaran21:gL1UEVxOEHQYIig7@cluster0.qkxzl.mongodb.net/forum?retryWrites=true&w=majority'
+  )
+  console.log('Connected to MongoDB')
+}
+
+connect().catch((err) => console.log(err))
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
