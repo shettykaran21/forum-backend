6177bafb9a349921cc467ccd83fd70e6f9810a38
Karan Shetty <shettykaran2110@gmail.com>
2021-11-23 16:30:33 +0530

    Sort users by created date

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,8 @@ const User = require('../models/user')
 
 exports.getUsers = async (req, res, next) => {
   try {
-    const users = await User.find()
+    const sortType = '-created'
+    const users = await User.find().sort(sortType)
     res.status(200).json(users)
   } catch (error) {
     if (!err.statusCode) {
