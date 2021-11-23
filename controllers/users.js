d7ac3d6e06414e5843eb519fd859714551cc02a0
Karan Shetty <shettykaran2110@gmail.com>
2021-11-23 16:50:03 +0530

    Get single user

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,27 @@ exports.getUsers = async (req, res, next) => {
     const sortType = '-created'
     const users = await User.find().sort(sortType)
     res.status(200).json(users)
-  } catch (error) {
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+      next(err)
+    }
+  }
+}
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const { username } = req.params
+
+    const user = await User.findOne({ username: username })
+
+    if (!user) {
+      const error = new Error('Could not find user')
+      error.statusCode = 404
+      throw error
+    }
+    res.status(200).json({ message: 'User found', user })
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500
       next(err)
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
-const { getUsers } = require('../controllers/users')
+const { getUsers, getUser } = require('../controllers/users')
 
 const router = express.Router()
 
 router.get('/', getUsers)
+
+router.get('/:username', getUser)
 
 module.exports = router
