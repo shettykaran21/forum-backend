dcf3ff14735dae6c8ebfee1d015eb1b1df6feef7
Karan Shetty <shettykaran2110@gmail.com>
2021-11-24 01:18:17 +0530

    Validation for question

--- routes/questions.js
+++ routes/questions.js
@@ -2,11 +2,12 @@ const express = require('express')
 
 const { getQuestions, createQuestion } = require('../controllers/questions')
 const isAuth = require('../middlewares/isAuth')
+const validateQuestion = require('../middlewares/validation/validateQuestion')
 
 const router = express.Router()
 
 router.get('/', getQuestions)
 
-router.post('/question', isAuth, createQuestion)
+router.post('/question', [isAuth, validateQuestion], createQuestion)
 
 module.exports = router
