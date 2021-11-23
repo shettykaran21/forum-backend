72154dba8601054c25cea1fd421840ca65a8a538
Karan Shetty <shettykaran2110@gmail.com>
2021-11-23 23:26:52 +0530

    Create question

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,8 +1,11 @@
+const { validationResult } = require('express-validator')
+
 const Question = require('../models/question')
 
 exports.getQuestions = async (req, res, next) => {
   try {
-    const questions = await Question.find()
+    const { sortType = '-score' } = req.body
+    const questions = await Question.find().sort(sortType)
     res.status(200).json(questions)
   } catch (error) {
     if (!err.statusCode) {
@@ -13,8 +16,24 @@ exports.getQuestions = async (req, res, next) => {
 }
 
 exports.createQuestion = async (req, res, next) => {
+  const errors = validationResult(req)
+
   try {
-    res.status(201).json({ message: 'Post created successfully' })
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed')
+      error.statusCode = 422
+      throw error
+    }
+
+    const { title, tags, text } = req.body
+    const author = req.user.id
+    const question = await Question.create({
+      title,
+      author,
+      tags,
+      text,
+    })
+    res.status(201).json({ message: 'Question posted successfully', question })
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500
