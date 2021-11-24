6f3483e16a6db70fd426a603385c0ef43291f5ed
Karan Shetty <shettykaran2110@gmail.com>
2021-11-24 16:59:48 +0530

    Route to search for tags

--- controllers/tags.js
+++ controllers/tags.js
@@ -34,3 +34,23 @@ exports.getPopularTags = async (req, res, next) => {
     next(err)
   }
 }
+
+exports.searchTags = async (req, res, next) => {
+  const { searchTag } = req.params
+  try {
+    const tags = await Question.aggregate([
+      { $project: { tags: 1 } },
+      { $unwind: '$tags' },
+      { $group: { _id: '$tags', count: { $sum: 1 } } },
+      { $match: { _id: { $regex: searchTag, $options: 'i' } } },
+      { $sort: { count: -1 } },
+    ])
+
+    res.status(200).json(tags)
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+}
--- routes/tags.js
+++ routes/tags.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
-const { getTags, getPopularTags } = require('../controllers/tags')
+const { getTags, getPopularTags, searchTags } = require('../controllers/tags')
 
 const router = express.Router()
 
 router.get('/', getTags)
 
 router.get('/popular-tags', getPopularTags)
+
+router.get('/:searchTag', searchTags)
 
 module.exports = router
