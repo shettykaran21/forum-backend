81d528ee21dda562d36919692a48d8c67e4eb1e6
Karan Shetty <shettykaran2110@gmail.com>
2021-11-24 16:43:53 +0530

    Route to get popular tags

--- controllers/tags.js
+++ controllers/tags.js
@@ -16,3 +16,21 @@ exports.getTags = async (req, res, next) => {
     next(err)
   }
 }
+
+exports.getPopularTags = async (req, res, next) => {
+  try {
+    const tags = await Question.aggregate([
+      { $project: { tags: 1 } },
+      { $unwind: '$tags' },
+      { $group: { _id: '$tags', count: { $sum: 1 } } },
+      { $sort: { count: -1 } },
+      { $limit: 25 },
+    ])
+    res.status(200).json(tags)
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+}
--- routes/tags.js
+++ routes/tags.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
-const { getTags } = require('../controllers/tags')
+const { getTags, getPopularTags } = require('../controllers/tags')
 
 const router = express.Router()
 
 router.get('/', getTags)
+
+router.get('/popular-tags', getPopularTags)
 
 module.exports = router
